3d595103a2eea144178d9bafb32be9a476e819d6
Will Martindale <wmartindale@gmail.com>
2022-02-08 20:45:04 -0800

    push first react app with jsx

---
+++ React/first-react-app-with-jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run the 4x4x48</li>
+        <li>Feed the cat</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
